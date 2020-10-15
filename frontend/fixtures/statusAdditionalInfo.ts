32d2937b55438f545965805a2ef354c4adf59e78
Hugo Holmqvist <hugo.holm@live.com>
2020-10-15 10:49:27 +0300

    Fix typo

--- frontend/fixtures/statusAdditionalInfo.ts
+++ frontend/fixtures/statusAdditionalInfo.ts
@@ -2,7 +2,7 @@ const additionalInfo: {[key: string]: string} = {
   great: 'The wind speed is very low and the ground is dry.',
   good: 'The wind speed is low and the ground is pretty dry.',
   average: `It's a little windy or the ground can be moist.`,
-  bad: `It' not a good idea.`
+  bad: `It's not a good idea.`
 }
 
 export default additionalInfo
